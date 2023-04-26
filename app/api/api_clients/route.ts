222f3ac14b40f818ed491cb01371902dbc7c55d6
ahmadshoaib-git <ahmad.shoaib88@gmail.com>
2023-04-26 23:52:18 +0500

    feat: orders apis added, api client and books api modified.

--- app/api/api_clients/route.ts
+++ app/api/api_clients/route.ts
@@ -4,7 +4,11 @@ import { ClientService } from '../../../services/client';
 
 export async function POST(request: NextRequest) {
     try {
+        // console.log(request);
         const req = await request.json();
+        // console.log(request.headers);
+        // const { authorization } = request.headers;
+        // console.log('authorization >', request.headers?.authorization || '');
         const { clientName, clientEmail } = req;
         const schema = Joi.object({
             clientName: Joi.string().min(3).max(255).required(),
@@ -17,11 +21,10 @@ export async function POST(request: NextRequest) {
             return NextResponse.json({ error: check.error?.details[0]?.message }, { status: 400 });
         }
         const checkClient = await ClientService.getClientByName(clientName);
-        if (checkClient && checkClient?.length > 0) {
-            return NextResponse.json({ error: `User with name:${name} already exists, please submit unique name.` }, { status: 404 });
-        }
-        const data = await ClientService.createAccessToken({ clientName, clientEmail });
-        return NextResponse.json(data);
+        if (!checkClient || checkClient?.length === 0) {
+            const data = await ClientService.createAccessToken({ clientName, clientEmail });
+            return NextResponse.json(data);
+        } else return NextResponse.json({ error: `User with name:${name} already exists, please submit unique name.` }, { status: 404 });
     } catch (err) {
         return NextResponse.json({ error: 'Something Went Wrong' }, { status: 500 });
     }
